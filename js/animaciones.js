4db785cbf03a99f188ad77de77b04322797417ce
busterlnk <franco@3volstudio.com>
2020-11-13 20:48:44 +0100

    Modificacion de botones de informacion

---
+++ js/animaciones.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.circulo').on('mouseenter', function(){
+        $(this).animate({
+            width: '175px',
+            height: '175px',
+            fontSize: '21px'
+        });
+    });
+    
+    $('.circulo').on('click', function(){
+        $(this).siblings().hide(); 
+        $(this).animate({
+            width: '100%',
+            height: '300px',
+            borderRadius: '2%'
+        });
+        $(this).parent().css({
+            width: '80%',
+            height: '100%'
+        });
+    });
+
+    $('.circulo').on('mouseleave', function(){
+        $(this).animate({
+            width: '150px',
+            height: '150px',
+            fontSize: '18px'
+        });
+        $(this).css({
+            position: 'relative',
+            borderRadius: '100%'
+        });
+        $(this).siblings().show();
+    });
+});
