547eb9f1dec8b3fb1aac12ab296379da1eb40787
busterlnk <franco@3volstudio.com>
2020-12-13 12:34:46 +0100

    botton send
    aun el formulario sigue sin funcionar, deberia trabajarlo con php

---
+++ js/form.js
@@ -0,0 +1,8 @@
+var email = document.getElementById('email').value;
+var message = document.getElementById('message').value;
+var submit = document.getElementById('submit');
+
+
+submit.addEventListener('click', function enviar(){
+
+});
